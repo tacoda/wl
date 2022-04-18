e6cbe6c6739aa5c7f9e48ae493b6bd1ddb068905
Ian Johnson <ijohnson@covermymeds.com>
2022-04-18 18:08:19 -0400

    Add structopt and reorganize

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use std::{env, process};
-use whois_rust::{WhoIs, WhoIsLookupOptions};
+use structopt::StructOpt;
+use whois_rust::{WhoIs, WhoIsError, WhoIsLookupOptions};
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "wl", about = "WhoIs Lookup")]
+struct Cli {
+    /// Host or IP address to lookup
+    #[structopt(name = "HOST")]
+    host: String,
+}
+
+fn whois() -> Result<WhoIs, WhoIsError> {
+    let json = r#"{
+        "org": "whois.pir.org",
+        "": "whois.ripe.net",
+        "_": {
+            "ip": {
+                "host": "whois.arin.net",
+                "query": "n + $addr\r\n"
+            }
+        }
+    }"#;
+    WhoIs::from_string(json)
+}
 
 fn main() {
-    let whois = WhoIs::from_path("/Users/ijohnson/servers.json").unwrap();
-    let args = env::args();
-    if args.len() < 2 {
-        eprintln!("Usage: wl <target>");
-        process::exit(1);
-    }
-    let target = env::args().last().unwrap();
-    let result: String = whois.lookup(WhoIsLookupOptions::from_string(target).unwrap()).unwrap();
+    let cli = Cli::from_args();
+    let whois = whois().unwrap();
+    let result = whois.lookup(WhoIsLookupOptions::from_string(cli.host).unwrap()).unwrap();
     println!("{}", result);
     process::exit(0);
 }
