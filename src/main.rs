437e4243c41e7fd0016d493f666116875d482558
Ian Johnson <ijohnson@covermymeds.com>
2022-04-18 16:35:15 -0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+use whois_rust::{WhoIs, WhoIsLookupOptions};
+
 fn main() {
-    println!("Hello, world!");
+    let whois = WhoIs::from_path("/Users/ijohnson/servers.json").unwrap();
+    let target = env::args().last().unwrap();
+    let result: String = whois.lookup(WhoIsLookupOptions::from_string(target).unwrap()).unwrap();
+    println!("{}", result);
 }
