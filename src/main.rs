3d7f21d10df5eac93798a2fa29d7093c2fe1c9e7
Ian Johnson <ijohnson@covermymeds.com>
2022-04-18 16:48:36 -0400

    Update usage to use arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::env;
+use std::{env, process};
 use whois_rust::{WhoIs, WhoIsLookupOptions};
 
 fn main() {
     let whois = WhoIs::from_path("/Users/ijohnson/servers.json").unwrap();
+    let args = env::args();
+    if args.len() < 2 {
+        eprintln!("Usage: wl <target>");
+        process::exit(1);
+    }
     let target = env::args().last().unwrap();
     let result: String = whois.lookup(WhoIsLookupOptions::from_string(target).unwrap()).unwrap();
     println!("{}", result);
+    process::exit(0);
 }
